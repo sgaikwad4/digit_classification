7857ca6a097f9eee1b8bef57e4c721d4914fd151
sgaikwad4 <siddhantg612@deltalearns.ca>
2026-03-09 13:51:42 -0700

    Added pseudocode
    Will add a lot more pseudocode later.

---
+++ digit_classification.py
@@ -0,0 +1,14 @@
+# Import Libraries
+
+# Make canvas for drawing
+
+# Drawing funciton
+
+# Neural network
+
+# Cost or loss function
+
+# Training function
+
+# Backpropagation (Updating weights) function
+
