9a9249d03b4f66cf7c661180e6f1001998354498
sgaikwad4 <siddhantg612@deltalearns.ca>
2026-03-11 14:45:38 -0700

    Created canvas for drawing
    made a drawing function so that the user can draw the number

--- digit_classification.py
+++ digit_classification.py
@@ -1,8 +1,40 @@
 # Import Libraries
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Make canvas for drawing
+# Drawing function
+drawing = False
+x_points = []
+y_points = []
 
-# Drawing funciton
+def on_press(event):
+    global drawing, x_points, y_points
+    drawing = True
+    x_points = []   # start a new line
+    y_points = []
+
+def on_release(event):
+    global drawing
+    drawing = False
+
+def on_move(event):
+    if drawing and event.xdata is not None and event.ydata is not None:
+        x_points.append(event.xdata)
+        y_points.append(event.ydata)
+
+        ax.plot(x_points, y_points, 'b-', linewidth=6.5)
+        fig.canvas.draw()
+
+fig, ax = plt.subplots()
+
+ax.set_xlim(0, 28)
+ax.set_ylim(0, 28)
+
+fig.canvas.mpl_connect('button_press_event', on_press)
+fig.canvas.mpl_connect('button_release_event', on_release)
+fig.canvas.mpl_connect('motion_notify_event', on_move)
+
+plt.show()
 
 # Neural network
 
