2b6c783315986d3f14853c2c34c2af70400d47c8
sgaikwad4 <siddhantg612@deltalearns.ca>
2026-03-12 09:55:26 -0700

    Function for clearing canvas
    Added a function for clearing canvas when "c" is pressed.

--- digit_classification.py
+++ digit_classification.py
@@ -7,23 +7,42 @@ drawing = False
 x_points = []
 y_points = []
 
+# Function for drawing when holding mouse button
 def on_press(event):
     global drawing, x_points, y_points
     drawing = True
     x_points = []   # start a new line
     y_points = []
 
+# Function to stop drawing when mouse is released
 def on_release(event):
     global drawing
     drawing = False
 
+# Draw when mouse is moving
 def on_move(event):
     if drawing and event.xdata is not None and event.ydata is not None:
         x_points.append(event.xdata)
         y_points.append(event.ydata)
 
-        ax.plot(x_points, y_points, 'b-', linewidth=6.5)
+        ax.plot(x_points, y_points,"black", linewidth=6.5)
         fig.canvas.draw()
+
+# Function to clear canvas
+def on_key(event):
+    global x_points, y_points
+    
+    if event.key == 'c':   # press 'c' to clear
+        ax.cla()           # clear the axes
+        ax.set_xlim(0, 28)
+        ax.set_ylim(0, 28)
+        
+        x_points = []
+        y_points = []
+        
+        fig.canvas.draw()
+
+
 
 fig, ax = plt.subplots()
 
